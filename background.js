d7976ca5811482dab30f11f965b4dea76e512956
dsin <phong.ek@gmail.com>
2013-07-01 00:54:32 +0700

    debugging javascript error

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
 var before_navigate_url = "";
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-  before_navigate_url = details.url;
+  before_navigate_url = details.url
 });
 
 chrome.webNavigation.onCompleted.addListener(function(details) {
-
+    
   chrome.storage.sync.get('URLmapping', function(items){
+
     var URLmapping = items['URLmapping']
     var redirectURL = URLmapping[details.url]
     if(redirectURL){
-      redirectURL.replace(/\[URL\]/g, details.url)
-      redirectURL.replace(/\[prevURL\]/g, before_navigate_url)    
-      redirectURL.replace(/\[prevURLwoHttp\]/g, before_navigate_url.indexOf('//')+2)
+      redirectURL = redirectURL.replace('[URL]', details.url)
+      redirectURL = redirectURL.replace('[prevURL]', before_navigate_url)    
+      redirectURL = redirectURL.replace('[prevURLwoHttp]', before_navigate_url.substring(before_navigate_url.indexOf('//')+2))
       chrome.tabs.update(details.tabId, {url: redirectURL});
     }
-  }
+  });
 
-  }
 });
